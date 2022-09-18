ba95f520ff0bdaf1163a78e54163c5b11f394241
Ebrahim <ebrahimtab81@gmail.com>
2022-09-18 22:52:06 +0430

    Initial commit

---
+++ src/ir/porteghali/javatutorial/E016_Variables.java
@@ -0,0 +1,27 @@
+package ir.porteghali.javatutorial;
+
+public class E016_Variables {
+
+    public void Start() {
+
+        //numeric data types
+        int a = 10;
+        short b = 5;
+        long c = 12345;
+        float d = 12.34f;
+        double e = 123456.789;
+
+        //characters
+        char f = 'a';
+        String g = "Ebrahim\nTab";
+
+        //boolean
+        boolean h = false;
+
+        System.out.println(a+b+c+d+e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+}
