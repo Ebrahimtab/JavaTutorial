3189f2cdb57fb1034258bd44a059ccf3314dc024
Ebrahim <ebrahimtab81@gmail.com>
2022-09-19 19:56:13 +0430

    Initial commit

---
+++ src/ir/porteghali/javatutorial/P05_Variables/E018_Keywords.java
@@ -0,0 +1,8 @@
+package ir.porteghali.javatutorial.P05_Variables;
+
+public class E018_Keywords {
+
+    public void Start() {
+
+    }
+}
---
+++ src/ir/porteghali/javatutorial/P06_Operators/E024_AssignmentOperators.java
@@ -0,0 +1,21 @@
+package ir.porteghali.javatutorial.P06_Operators;
+
+public class E024_AssignmentOperators {
+
+    public void Start() {
+
+        /*
+            =
+            +=
+            -=
+            *=
+            /=
+            %=
+         */
+
+        int a = 5;
+        a += 4;
+
+        System.out.println(a);
+    }
+}
