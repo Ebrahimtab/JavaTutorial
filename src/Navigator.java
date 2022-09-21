f1862558444e6d5c87cdad7605095b85896b7340
Ebrahim <ebrahimtab81@gmail.com>
2022-09-22 01:46:25 +0330

    Initial commit

--- src/Navigator.java
+++ src/Navigator.java
@@ -1,8 +1,3 @@
-import ir.porteghali.javatutorial.P05_Variables.E020_TryCatchBlock;
-import ir.porteghali.javatutorial.P08_Classes.E030_Instance_Example;
-import ir.porteghali.javatutorial.P09_FilesAndThreads.E035_Threads;
-import ir.porteghali.javatutorial.P09_FilesAndThreads.E036_Files;
-import ir.porteghali.javatutorial.P09_FilesAndThreads.E037_WriteFile;
 import ir.porteghali.javatutorial.P09_FilesAndThreads.E038_ReadFile;
 
 /**
