1dc8592b77a0a8b92f9a72fdd59a6b49faf1283b
Ebrahim <ebrahimtab81@gmail.com>
2022-09-24 15:24:07 +0330

    Initial commit

--- src/ir/porteghali/javatutorial/p11_SimpleProject/E057_SimpleProject.java
+++ src/ir/porteghali/javatutorial/p11_SimpleProject/E057_SimpleProject.java
@@ -1,5 +1,8 @@
 package ir.porteghali.javatutorial.p11_SimpleProject;
 
+import com.sun.xml.internal.stream.events.EndDocumentEvent;
+
+import javax.xml.stream.events.EndDocument;
 import java.util.Scanner;
 
 public class E057_SimpleProject {
@@ -7,6 +10,8 @@ public class E057_SimpleProject {
     int a;
     int b;
     String operator;
+    int respose;
+    int operatorError = 0;
     Scanner scanner;
     public E057_SimpleProject() {
         System.out.println("Welcome to my great calculator!!!");
@@ -25,29 +30,41 @@ public class E057_SimpleProject {
         b = Integer.parseInt(scanner.nextLine());
         
         E057_Calculator calculator = new E057_Calculator();
-        int respose;
-        int operatorError = 0;
+
         switch (operator) {
             case "+" : {
                 respose = calculator.sum(a,b);
+                this.answer();
                 break;
             }
             case "-" : {
                 respose = calculator.minus(a,b);
+                this.answer();
                 break;
             }
             case "*" : {
                 respose = calculator.mult(a,b);
+                this.answer();
                 break;
             }
             case "/" : {
-                respose = calculator.div(a,b);
+                if (b != 0) {
+                    respose = calculator.div(a,b);
+                    this.answer();
+                }
+                else {
+                    System.out.println("We can not division by zero");
+                }
                 break;
             }
             default:
                 respose = 0;
                 operatorError = 1;
+                this.answer();
         }
+    }
+
+    public void answer() {
 
         if (operatorError == 0) {
             System.out.println("The answer:");
